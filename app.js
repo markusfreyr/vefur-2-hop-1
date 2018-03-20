6cd14923fb4a95eae6c85ba33f16952a4240624d
Freyr Danielsson <freyrdanielsson@gmail.com>
2018-03-20 13:10:08 +0000

    update error handling appjs

--- app.js
+++ app.js
@@ -19,23 +19,22 @@ app.use('/users', users);
 app.use('/books', books);
 app.use('/categories', categories);
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+function notFoundHandler(req, res, next) { // eslint-disable-line
+  res.status(404).json({ error: 'Not found' });
+}
 
-// error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+function errorHandler(err, req, res, next) { // eslint-disable-line
+  console.error(err);
 
-  // render the error page
-  res.status(err.status || 500);
-  // res.render('error');
-});
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    return res.status(400).json({ error: 'Invalid json' });
+  }
+
+  return res.status(500).json({ error: 'Internal server error' });
+}
+
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 const {
   PORT: port = 3000,
